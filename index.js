2cf2fe4d6cb2e9522e0c92b34854d178bababc95
Dhruba Chatterjee <dhrubach@hotmail.com>
2017-05-08 17:59:59 +0530

    update(webpack) : include support for ES6 and sass compilation

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-document.write("hello, webpack!!!");
+(() => {
+    'use strict';
+
+    require('./styles.scss');
+
+    class WebpackStarter {
+        static Display(message) {
+            document.write(message);
+        }  
+    }
+
+    WebpackStarter.Display('webpack with babel-loader for ES6 support..');
+
+})();
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,19 @@ let config = {
     entry: './index.js',
     output: {
         filename: './dist/output.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                loader: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
     }
 };
 
