a64936526c8ac43e17a30154547868359d0bae4c
Dhruba Chatterjee <dhrubach@hotmail.com>
2017-06-29 14:21:35 +0530

    feat(styles) : add sass and postcss support

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 
 'use strict';
 
-//require('./styles.scss');
+require('./styles.scss');
 
 require('lodash');
 
--- webpack.config.js
+++ webpack.config.js
@@ -8,19 +8,19 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 let config = {
-    entry: './src/main.js',
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: '[name].[chunkhash].js',
-    },
-    module: {
-        rules: [
+	entry: './src/main.js',
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: '[name].[chunkhash].js',
+	},
+	module: {
+		rules: [
 			{
 				test: /\.ts$/,
 				exclude: /node_modules/,
 				use: [
-					{ loader: 'babel-loader'},
-					{ loader: 'ts-loader'},
+					{ loader: 'babel-loader' },
+					{ loader: 'ts-loader' },
 				],
 			},
 			{
@@ -33,52 +33,60 @@ let config = {
 					formatter: 'grouped'
 				}
 			},
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.scss$/,
-                use: ExtractTextWebpackPlugin.extract({
-                    use: ['css-loader', 'sass-loader'],
-                    fallback: 'style-loader'
-                })
-            }
-        ]
-    },
-    plugins: [
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor',
-            minChunks: function (module) {
-                return module && module.context && module.context.indexOf('node_modules') > -1;
-            },
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/index-template.ejs',
-            title: 'Webpack v2 Basic Starter',
-            filename: 'index.html'
-        }),
-        new InlineChunkManifestHtmlWebpackPlugin(),
-        new ExtractTextWebpackPlugin('styles.css'),
-    ],
-    devServer: {
-        contentBase: path.resolve(__dirname, './dist'),
-        historyApiFallback: true,
-        inline: true,
-        open: true,
-    },
-    resolve: {
-        extensions: ['.js', '.ts', '.scss'],
-    },
-    devtool: 'cheap-module-source-map',
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			},
+			{
+				test: /\.scss$/,
+				use: ExtractTextWebpackPlugin.extract({
+					use: [
+						{ loader: 'css-loader' },
+						{ loader: 'resolve-url-loader' },
+						{
+							loader: 'sass-loader',
+							options: { sourcemap: true },
+						},
+						{ loader: 'postcss-loader' },
+					],
+					fallback: 'style-loader',
+				})
+			},
+		]
+	},
+	plugins: [
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'vendor',
+			minChunks: function (module) {
+				return module && module.context && module.context.indexOf('node_modules') > -1;
+			},
+		}),
+		new HtmlWebpackPlugin({
+			template: './src/index-template.ejs',
+			title: 'Webpack v2 Basic Starter',
+			filename: 'index.html'
+		}),
+		new InlineChunkManifestHtmlWebpackPlugin(),
+		new ExtractTextWebpackPlugin('styles.css'),
+	],
+	devServer: {
+		contentBase: path.resolve(__dirname, './dist'),
+		historyApiFallback: true,
+		inline: true,
+		open: true,
+	},
+	resolve: {
+		extensions: ['.js', '.ts', '.scss'],
+	},
+	devtool: 'cheap-module-source-map',
 };
 
 module.exports = config;
 
 if (process.env.NODE_ENV === 'production') {
-    module.exports.plugins.push(
-        new webpack.optimize.UglifyJsPlugin(),
-        new OptimizeCssAssetsPlugin()
-    );
+	module.exports.plugins.push(
+		new webpack.optimize.UglifyJsPlugin(),
+		new OptimizeCssAssetsPlugin()
+	);
 }
